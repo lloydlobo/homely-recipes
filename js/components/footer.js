aa99d7deaf48dffc0003124e067eecf2398f8c23
Lloyd Lobo <lloyd@lloydlobo.com>
2022-03-30 03:13:19 +0530

    Add footer component shadowDOM element
    Style it after adding the logo

---
+++ js/components/footer.js
@@ -0,0 +1,38 @@
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          display: block;
+          padding: 1rem;
+          background-color: #f5f5f5;
+        }
+        :host ul.navigation {
+          display: flex;
+          list-style: none;
+          gap: 1rem;
+          justify-content: center;
+          align-items: center;
+          padding: 0;
+
+        }
+      </style>
+      <slot>
+      <ul class="navigation">
+        <li>Home</li>
+        <li>About</li>
+        <li>Product</li>
+        <li>Pricing</li>
+        <li>Contact</li>
+      </ul>
+      </slot>
+    `;
+  }
+}
+
+// Register the new element with the browser.
+window.customElements.define("footer-component", Footer);
+("use strict");
