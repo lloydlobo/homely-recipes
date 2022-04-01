96b4513c76b4b3976169dcba32e7d44c9a8e76e4
Lloyd Lobo <lloyd@lloydlobo.com>
2022-04-01 07:06:21 +0530

    Feat: Autocomplete search bar with array items

--- js/script.js
+++ js/script.js
@@ -1,30 +1,102 @@
-// create function siteSearch to fetch the value of input id="inputSearchSite" and execute with button onclick to display the value in the element with id="recipeList"
+// * Autocomplete Search
+// this could be the result of an API
+let searchable = [
+  "Apple",
+  "Avocado",
+  "Banana",
+  "Orange",
+  "Strawberry",
+  "Blueberry",
+  "Raspberry",
+  "Blackberry",
+  "Grape",
+  "Peach",
+  "Cherry",
+  "Mango",
+  "Pineapple",
+  "Kiwi",
+  "Coconut",
+  "Papaya",
+  "Pear",
+  "Plum",
+  "Apricot",
+  "Lemon",
+  "Lime",
+  "Grapefruit",
+  "Watermelon",
+];
 
-function searchSite() {
-  const searchValue = document.getElementById("inputSearchSite").value;
-  // Manipulate the DOMstring with the <p id="recipeList"> element
-  // Add the searchValue to the DOMstring
-  document.getElementById(
-    "recipeList"
-  ).innerHTML = `<h4>Showing results for: ${searchValue}</h4>`;
+const searchInput = document.getElementById("search");
+const searchWrapper = document.querySelector(".wrapper");
+const resultsWrapper = document.querySelector(".results");
+
+// add event listener for search input events
+searchInput.addEventListener("keyup", () => {
+  // // console.log(e.target.value);
+  // filter through our searchable variable's itemListElement
+  let inputValue = searchInput.value;
+
+  // check the length of the input
+  // console.log(inputValue.length);
+  if (inputValue.length) {
+    results = searchable.filter((item) => {
+      return item.toLowerCase().includes(inputValue.toLowerCase());
+    });
+  }
+  // renderResults of results
+  renderResults(results);
+});
+
+// renders the results
+function renderResults(results) {
+  // if there is no items or no results.length - remove the class .show
+  if (!results.length) {
+    return searchWrapper.classList.remove("show");
+  }
+
+  // when there are results, loop over results with callback funtion map of item
+  let content = results
+    .map((item) => {
+      return `<li><a href="">${item}</a></li>`;
+      // return `<li><a href="">${item.name}</a></li>`;
+    })
+    .join(``); /* join this on a string */
+  // console.log(content);/* pass the console.log inside the function -- it grabs the results if there is length -- loops over the results and pass it through a map function */
+  searchWrapper.classList.add("show");
+  resultsWrapper.innerHTML = `<ul>${content}</ul>`;
 }
 
+// //
+// //
+// //
+// create function siteSearch to fetch the value of input id="inputSearchSite" and execute with button onclick to display the value in the element with id="recipeList"
+
+// function searchSite() {
+//   const searchValue = document.getElementById("inputSearchSite").value;
+//   // Manipulate the DOMstring with the <p id="recipeList"> element
+//   // Add the searchValue to the DOMstring
+//   document.getElementById(
+//     "recipeList"
+//   ).innerHTML = `<h4>Showing results for: ${searchValue}</h4>`;
+// }
+
 // Run function searchSite when keyup is "Enter"
-document
-  .getElementById("inputSearchSite")
-  .addEventListener("keyup", function (event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.key === "Enter") {
-      // trigger the function searchSite
-      // displaySearchSiteResults();
-      searchSite();
-    }
-  });
+// document
+//   .getElementById("inputSearchSite")
+//   .addEventListener("keyup", function (event) {
+//     // Number 13 is the "Enter" key on the keyboard
+//     if (event.key === "Enter") {
+//       // trigger the function searchSite
+//       // displaySearchSiteResults();
+//       searchSite();
+//     }
+//   });
 
 // // //
 // // //
 // // //
 // // //
+
 // // //
 // // //
 // // //
