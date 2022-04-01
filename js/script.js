e9d2f5c4e2aa8cf5b1c6f6dd2807d5e8c66e2eef
Lloyd Lobo <lloyd@lloydlobo.com>
2022-04-01 14:38:30 +0530

    Refactor code and remove comments

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
 // * Autocomplete Search
-// this could be the result of an API
-// const searchable = [
-//   "Fish Curry Special",
-//   "Special Chicken Masala",
-//   "Traditional Rava Laddo",
-//   "Bread Butter Puding",
-//   "Rice Neer Dosa",
-// ];
 
 const searchable = [
   "Fish Curry Special",
@@ -22,35 +14,28 @@ const searchable = [
 ];
 
 //  maps out searchable sub-arrays
-
 searchable.map(function (subarray) {
   console.log(subarray);
 });
-// //
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#grouping_the_elements_of_an_array
 
 const searchInput = document.getElementById("search");
 const searchWrapper = document.querySelector(".wrapper");
 const resultsWrapper = document.querySelector(".results");
 
-// add event listener for search input events
 searchInput.addEventListener("keyup", () => {
   // // console.log(e.target.value);
   // filter through our searchable variable's itemListElement
   const inputValue = searchInput.value;
-
-  // check the length of the input
-  // console.log(inputValue.length);
+  // console.log(inputValue.length);   /* check the length of the input */
   if (inputValue.length) {
     results = searchable.filter((item) => {
       return item.toLowerCase().includes(inputValue.toLowerCase());
     });
   }
-  // renderResults of results
   renderResults(results);
 });
 
-// renders the results
+// renders the results of filtered variable `searchable` array
 function renderResults(results) {
   // if there is no items or no results.length - remove the class .show
   if (!results.length) {
@@ -66,13 +51,7 @@ function renderResults(results) {
       // return `<li><a href="collections/${item.collection}/${item.recipe}.html">${item}</a></li>`;
     })
     .join(""); /* join this on a string */
-  // console.log(content);
-  /* pass the console.log inside the function -- it grabs the results if there is length -- loops over the results and pass it through a map function */
+  // console.log(content); /* pass the console.log inside the function -- it grabs the results if there is length -- loops over the results and pass it through a map function */
   searchWrapper.classList.add("show");
   resultsWrapper.innerHTML = `<ul>${content}</ul>`;
 }
-// function search(term) {
-//   return searchable.filter((recipe) => {
-//     return recipe.recipe.toLowerCase().includes(term.toLowerCase());
-//   });
-// }
