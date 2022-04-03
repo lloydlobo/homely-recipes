47439e6acfb14a8f90dc07459ecff24da62a9e51
Lloyd Lobo <lloyd@lloydlobo.com>
2022-04-03 19:51:46 +0530

    Update links to disable with msg comming soon

--- js/components/navigation.js
+++ js/components/navigation.js
@@ -3,11 +3,11 @@
 // Path: js/components/navigation.js
 // Compare this snippet from js/components/Footer.js:
 class Navigation extends HTMLElement {
-  constructor () {
-    super()
+  constructor() {
+    super();
 
     // Allow the shadowDOM to be styled by external CSS with the mode: open
-    this.attachShadow({ mode: 'open' })
+    this.attachShadow({ mode: "open" });
 
     this.shadowRoot.innerHTML = `
       <style>
@@ -96,24 +96,53 @@ class Navigation extends HTMLElement {
           text-decoration: none;
         }
 
+        :host a.disabled {
+          pointer-events: none;
+            --mdc-theme-outline: #857371;
+          color:   var(--mdc-theme-outline, #857371);
+            position: relative;
+        }
+
+        :host li.disabled {
+          position: relative;
+
+        }
+
+        :host ul.navigation li.disabled:hover {
+          text-decoration: none;
+          position: relative;
+          background: transparent;
+        }
+       :host ul.navigation li.disabled:hover::after {
+          content: "coming soon...";
+          position: absolute;
+          color: #857371;
+          background: #f4dddb;
+          top: -23px;
+          right: -12px;
+          font-size: smaller;
+          padding-inline: .5ch;
+          border-radius: 0.25rem;
+        }
+
       </style>
 
       <slot>
         <div class="navbar">
           <ul class="navigation | body-small">
-            <li>Collections</li>
-            <li>Holiday & Seasons</li>
+            <li><a href="collections.html">Collections</a></li>
+            <li class="disabled"><a class="disabled" href="holiday-seasons-recipes.html" aria-disabled="true">Holiday & Seasons</a></li>
             <li><a href="about.html">About</a></li>
             <li><a href="contact.html">Contact</a></li>
           </ul>
         </div>
       </slot>
-    `
+    `;
   }
 }
 
 // Register the new element with the browser.
-window.customElements.define('navigation-component', Navigation)
+window.customElements.define("navigation-component", Navigation);
 
 // function searchResults() {
 //   const searchSiteInput = document.getElementById(searchSite).value;
