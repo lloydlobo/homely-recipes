baa1ae3f4930b8eb82b05b039e4aa1bdf65914a1
Lloyd Lobo <lloyd@lloydlobo.com>
2022-04-05 11:40:38 +0530

    Update logo component and footer

--- js/components/logo.js
+++ js/components/logo.js
@@ -1,23 +1,53 @@
+// Call a constant logoInnerHTML that which contain the innerHTML for shadowRoot
+const logoInnerHTML = `
+<link rel="stylesheet" href="../../css/style.css">
+<link rel="stylesheet" href="./css/style.css">
+
+<style>
+:host * {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+:host .logo {
+  font-size: 1.5rem;
+  font-family: Parisienne, cursive;
+  transform: rotate(351.65deg);
+  --mdc-theme-primary: #9c413d;
+  color: var(--mdc-theme-primary);
+}
+
+:host .logo a {
+  text-decoration: none;
+}
+
+:host .logo:hover {
+  cursor: pointer;
+}
+
+</style>
+
+
+      <slot>
+      <div class="logo">
+      <span><a href=/index.html>Homely</a></span>
+      </div>
+
+      </slot>
+
+`;
+
 // Add a shadowDOM class Logo for logo witht he constructor() function
 
 class Logo extends HTMLElement {
   constructor() {
+    // Always call super first in constructor
     super();
 
     // Allow the shadowDOM to be styled by external CSS with the mode: open
     this.attachShadow({ mode: "open" });
-    this.shadowRoot.innerHTML = `
-      <link rel="stylesheet" href="/css/style.css">
-      <link rel="stylesheet" href="/css/logo.css">
-
-      <slot>
-
-      <div class="logo">
-      <span><a href=index.html>Homely</a></span>
-      </div>
-
-      </slot>
-        `;
+    this.shadowRoot.innerHTML = logoInnerHTML;
   }
 }
 
