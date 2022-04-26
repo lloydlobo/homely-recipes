96f69a914350c40d9d0788072baa9547a5fcd5a6
Lloyd Lobo <lloyd@lloydlobo.com>
2022-04-26 17:47:49 +0530

    ci: Init npm webpack, eslint, browser-sync

---
+++ src/generateJokes.js
@@ -0,0 +1,9 @@
+// eslint ./node_modules/.bin/eslint --fix
+
+// * generate jokes
+function generateJokes() {
+  return "I don't trust stairs. They're always up to something.";
+}
+
+// export default generateJokes; script npm run build makes webpack build main.js in /dist
+export default generateJokes;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-console
+// console.log("Gloria In Excelsis Deo!");
+import generateJokes from "./generateJokes";
+console.log(generateJokes());
+
+/* const newPara = document.createElement("p");
+newPara.classList.add("h1");
+newPara.textContent = "Gloria In Excelsis Deo!";
+// add newPara to the DOM
+document.body.appendChild(newPara);
+ */
