9cb9c09ee378600cb04f7c656083213676fb2c6a
Lloyd Lobo <lloyd@lloydlobo.com>
2022-04-27 11:21:20 +0530

    style: Format with double quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: ['airbnb-base'],
+  extends: ["airbnb-base"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-    semi: ['error', 'always'],
-    quotes: ['error', 'double']
-  }
-}
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+  },
+};
 
 // https://eslint.org/docs/user-guide/getting-started
