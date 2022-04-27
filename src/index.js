0646c37317f7f97d45647cbefa2acc768fba66d2
Lloyd Lobo <lloyd@lloydlobo.com>
2022-04-27 10:41:10 +0530

    refactor: Replace h2 with h1

--- src/index.js
+++ src/index.js
@@ -2,21 +2,16 @@ import laughing from "./assets/laughing.svg";
 import generateJokes from "./generateJokes";
 import "./styles/main.scss"; /* You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders */
 
+// eslint-disable-next-line no-console
+// console.log("Gloria In Excelsis Deo!");
+
 // add loader in webpack.config.js
 // add a img id element in template.html
 const laughingImg = document.getElementById("laughingImg");
 laughingImg.src = laughing;
-// eslint-disable-next-line no-console
-console.log("Gloria In Excelsis Deo!");
 
-// eslint-disable-next-line no-console
-console.log(generateJokes());
-// eslint-disable-next-line no-console
-console.log("Change");
+const btnGenerateJokes = document.getElementById("btnJoke");
+btnGenerateJokes.addEventListener("click", generateJokes);
 
-/* const newPara = document.createElement("p");
-newPara.classList.add("h1");
-newPara.textContent = "Gloria In Excelsis Deo!";
-// add newPara to the DOM
-document.body.appendChild(newPara);
- */
+// Call imported function `generateJokes` once from "./generateJokes.js"
+generateJokes();
