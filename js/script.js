d33938c17674bcdb9ab60bf6ada0f9b0f7780528
Lloyd Lobo <lloyd@lloydlobo.com>
2022-04-27 12:02:18 +0530

    refactor: Split large functions to atomic ones

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-// ****
-// * Autocomplete Search
-// ****
+// Autocomplete Search Array
 const searchable = [
   "Bread Butter Pudding",
   "Fish Curry Special",
@@ -17,14 +15,15 @@ const searchableLink = [
   "collections/rava-recipes/traditional-rava-laddo.html",
 ];
 
+let searchResults;
+
 // filter the searchableLink array
 function filterResults(e) {
   const searchTerm = e.target.value.toLowerCase();
-  const searchResults = searchable.filter((item) => {
-    return item.toLowerCase().includes(searchTerm);
-  });
+  searchResults = searchable.filter((item) => item.toLowerCase().includes(searchTerm));
   const searchResultsLink = searchableLink.filter((item) => {
-    return item.toLowerCase().includes(searchTerm);
+    item.toLowerCase().includes(searchTerm);
+    return item;
   });
   const searchResultsList = document.querySelector(".search-results");
   searchResultsList.innerHTML = "";
@@ -33,63 +32,55 @@ function filterResults(e) {
     li.innerHTML = `<a href="${searchResultsLink[index]}">${item}</a>`;
     searchResultsList.appendChild(li);
   });
-  filterResults(searchResults);
+  return searchResults;
 }
 
-const recipeAndLink = { searchable, searchableLink };
-console.log(recipeAndLink);
+filterResults(searchResults);
 
-// *
 // * set variables from the DOM
-// *
 const searchInput = document.getElementById("search");
 const searchWrapper = document.querySelector(".wrapper");
 const resultsWrapper = document.querySelector(".results");
 
-searchInput.addEventListener("keyup", () => {
-  // // console.log(e.target.value);
-  // filter through our searchable variable's itemListElement
-  const inputValue = searchInput.value;
-  if (inputValue.length) {
-    results = searchable.filter((item) => {
-      return item.toLowerCase().includes(inputValue.toLowerCase());
-    });
-  }
-  renderResults(results);
-});
+// function to create the list items
+function addSearchResultContent(item) {
+  const searchableLinkItem = searchableLink[1]; // filter with results index
+  return `<li><a href="${searchableLinkItem}">${item}</a></li>`;
+}
 
-// *
-// * renders the results of filtered variable `searchable` array
-// *
-function renderResults(results) {
-  // if there is no items or no results.length - remove the class .show
-  if (!results.length) {
-    return searchWrapper.classList.remove(
-      "show"
-    ); /* ? could it be toggle instead of remove? */
-  }
-  // * [ ] filter the searchableLink.map with results index /* todo */
-  const searchableLinkItem = searchableLink[1];
-  // when there are results, loop over results with callback function map of item
-  const content = results
-    .map((item) => {
-      return `<li><a href="${searchableLinkItem}">${item}</a></li>`;
-      // return `<li><a href="collections/${item.collection}/${item.recipe}.html">${item}</a></li>`;
-    })
-    .join(""); /* join this on a string */
-
+// Function to show the results by adding the class `show` to the wrapper
+// and adding the content to the results wrapper
+function showSearchResults(content) {
   searchWrapper.classList.add("show");
   resultsWrapper.innerHTML = `<ul>${content}</ul>`;
 }
 
-// when a <li> is clicked and searchable filter item's array index matches searchableLink,
-// append the link to the searchableLink array index
+// Function renders the results of filtered variable `searchable` array
+function renderResults(results) {
+  if (!results.length) {
+    return searchWrapper.classList.remove("show");
+  }
+  // Function to Loop over results with callback function map of item
+  const content = results.map((item) => addSearchResultContent(item)).join("");
 
-// resultsWrapper.addEventListener("click", (e) => {
-//   if (e.target.item == searchableLink[0]) {
-//     const index = searchable.indexOf(e.target.textContent);
-//     window.location.href = searchableLink[index];
-//   } else {
-//     return;
-//   } /* ? */
-// });
+  const showsSearchResultsContent = showSearchResults(content);
+
+  return showsSearchResultsContent;
+}
+
+searchInput.addEventListener("keyup", (results) => {
+  // filter through our searchable variable's itemListElement
+  let resultsValue = results;
+  const inputValue = searchInput.value;
+  if (inputValue.length) {
+    resultsValue = searchable.filter((item) => {
+      item.toLowerCase().includes(inputValue.toLowerCase());
+
+      return item;
+    });
+    return resultsValue;
+  }
+  const renderedResults = renderResults(resultsValue);
+
+  return renderedResults;
+});
