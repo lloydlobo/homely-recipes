d821d45ee6909a3753afd65d5da4a253589945a2
Lloyd Lobo <lloyd@lloydlobo.com>
2022-04-27 18:47:17 +0530

    build: Re-build with scss in mind

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
+// Gloria In Excelsis Deo!
+// assets
 import laughing from "./assets/laughing.svg";
-import generateJokes from "./generateJokes";
+// components
+import Footer from "./components/footer";
+import Navigation from "./components/navigation";
+// functions
+import generateJokes from "./functions/generateJokes";
+// styles
 import "./styles/scss/main.scss";
 
-// console.log("Gloria In Excelsis Deo!");
-
+// **** generate jokes card **** /* this can be a component */
 const laughingImg = document.getElementById("laughingImg");
 const btnGenerateJokes = document.getElementById("btnJoke");
-
 laughingImg.src = laughing;
-btnGenerateJokes.addEventListener("click", generateJokes);
 
-// Call imported function `generateJokes` once from "./generateJokes.js"
-generateJokes();
+btnGenerateJokes.addEventListener("click", generateJokes);
+generateJokes(); // Call imported function `generateJokes` once
+// **** end of generate jokes card ****
+
+// **** shadowDOM Custom Elements ****
+// window.customElements.define("navigation-component", Navigation);
