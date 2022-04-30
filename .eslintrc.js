d206e6384845129ad4604ad8c5e5c83bf940c967
Lloyd Lobo <lloyd@lloydlobo.com>
2022-04-30 11:46:29 +0530

    ci: set eslint rules for jest & ignore netlify

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,22 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "jest/globals": true,
   },
   extends: ["airbnb-base", "plugin:jsx-a11y/recommended"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["jsx-a11y"],
+  plugins: ["jsx-a11y", "jest"],
   rules: {
     semi: ["error", "always"],
     quotes: ["error", "double"],
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
   },
 };
 
